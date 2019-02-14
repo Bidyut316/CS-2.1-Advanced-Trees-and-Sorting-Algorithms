987461d7d0851e207fd0b9136f022d9db756b2c1
Novandev <donovan.adams@students.makeschool.com>
2019-02-14 15:09:23 -0800

    refactoring from scratch

--- autocommplete/autocomplete.py
+++ autocommplete/autocomplete.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python
+from charNode import CharNode
 class Autocomplete(object):
+    """ This class will iplement the CharNode class to attempt to complete and autocomplete will most likely redo this in python"""
     def __init__(self):
+        self.root = CharNode()
+        self.dictionary= [word.replace('\n', '') for word in open("/usr/share/dict/words", "r").readlines()]
 
+
+
+if __name__ == "__main__":
+    test = Autocomplete()
+    print(test.dictionary)
--- autocommplete/charNode.py
+++ autocommplete/charNode.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python
 class CharNode(object):
-    def __init__(self):
+    """This class keeps track of if a node is at the end of the tree of possible combinations"""
+    def __init__(self, label=None, data=None):
         self.isEnd = False
+        self.childNodes = dict()
+
+
+    def addChild(self, key, data=None):
+        if not isinstance(key, Node):
+            self.children[key] = Node(key, data)
+        else:
+            self.children[key.label] = key
