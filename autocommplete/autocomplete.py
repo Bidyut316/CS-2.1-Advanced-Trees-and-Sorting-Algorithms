bd13f46beef98ab5726b9e2ea52b9a05ac024c50
Novandev <donovan.adams@students.makeschool.com>
2019-02-16 21:08:57 -0800

    added the tries from my other repo in here

--- autocommplete/autocomplete.py
+++ autocommplete/autocomplete.py
@@ -4,10 +4,16 @@ class Autocomplete(object):
     """ This class will iplement the CharNode class to attempt to complete and autocomplete will most likely redo this in python"""
     def __init__(self):
         self.root = CharNode()
-        self.dictionary= [word.replace('\n', '') for word in open("/usr/share/dict/words", "r").readlines()]
+        # self.dictionary= [word.replace('\n', '') for word in open("/usr/share/dict/words", "r").readlines()]
+        self.dictionary =['app','apple', 'application', 'apply','apricot']
+    def get_possisble_words(self,word:str):
+        for word in self.dictionary:
+            print(word)
+        print(type(self.root))
+        
 
 
 
 if __name__ == "__main__":
     test = Autocomplete()
-    print(test.dictionary)
+    print(test.root.children)
--- autocommplete/charNode.py
+++ autocommplete/charNode.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
+
+
+
+def char_range(c1, c2):
+    """Generates the characters from `c1` to `c2`, inclusive."""
+    for c in range(ord(c1), ord(c2)+1):
+        yield chr(c)
+
+
 class CharNode(object):
     """This class keeps track of if a node is at the end of the tree of possible combinations"""
-    def __init__(self, label=None, data=None):
-        self.isEnd = False
-        self.childNodes = dict()
 
+    def __init__(self):
+        self.next = dict() # Initialize and empty python dictionary that will hold references to
 
-    def addChild(self, key, data=None):
-        if not isinstance(key, Node):
-            self.children[key] = Node(key, data)
-        else:
-            self.children[key.label] = key
+        self.word_marker = False
