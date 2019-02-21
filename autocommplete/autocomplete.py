67a760c0abf43d0a3fc534a8fbf570525349fe24
Novandev <donovan.adams@students.makeschool.com>
2019-02-21 14:48:38 -0800

    cleaning up

--- autocommplete/autocomplete.py
+++ autocommplete/autocomplete.py
@@ -1,44 +1,15 @@
 #!/usr/bin/python
-import pprint
+
 from trie import Trie
 class Autocomplete(object):
     """ This class will iplement the CharNode class to attempt to complete and autocomplete will most likely redo this in python"""
     def __init__(self):
         self.root = Trie()
-        # self.dictionary= [word.replace('\n', '') for word in open("/usr/share/dict/words", "r").readlines()]
-        self.dictionary =['app','apple', 'application', 'apply','apricot']
+        self.dictionary= [word.replace('\n', '') for word in open("/usr/share/dict/words", "r").readlines()]
+        # self.dictionary =['app','apple', 'application', 'apply','apricot']
         for word in self.dictionary:
             self.root.add(word)
 
-
-    def autocomplete(self,curr=None,word='',autocomplete_str = ''):
-
-        if curr is None:
-            curr = self.root.head
-
-        for char in word:
-            # print(curr.keys())
-            if '*' in curr:
-                print("eno of word")
-            if char in curr:
-                autocomplete_str += char
-                curr = curr[char]
-                print(autocomplete_str)
-            # if len(curr)
-
-    def add_to_autostring(self, add_to_auto_str='', curr_branch=None):
-        # add_to_auto_str +=
-        print(curr_branch)
-        for letter, children in curr_branch.items():
-            if letter == '*':
-                add_to_auto_str += ' ' + add_to_auto_str
-                yield add_to_auto_str
-            else:
-                # yield letter
-                # print(children)
-                add_to_auto_str += letter
-                # yield add_to_auto_str
-                yield from self.add_to_autostring(add_to_auto_str,children)
 
 
     def autocompleteTest(self, word):
--- autocommplete/trie.py
+++ autocommplete/trie.py
@@ -1,5 +1,5 @@
+#!/usr/bin/python
 
-import pprint
 from charNode import CharNode
 class Trie:
     def __init__(self):
