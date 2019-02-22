761fc281690c51f99bd985ba3d4c5dcde4c8f00d
Novandev <donovan.adams@students.makeschool.com>
2019-02-21 16:51:16 -0800

    adding rb tree to the repo

---
+++ redblack/redBlack.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+class RBNode(object):
+    def __init__(self):
+        self.color=''
+        self.left = None
+        self.right = None
+
+class RedBlackTree(object):
+    """
+    Class for implementing a standard red-black trees
+    """
+    def __init__(self):
+        self.root =RBNode()
+
+    def add(self):
+        pass
+    def delete(self):
+        pass
+    def get_all_nodes(self):
+        pass
+    def get_red_nodes(self):
+        pass
+    def get_black_nodes(self):
+        pass
